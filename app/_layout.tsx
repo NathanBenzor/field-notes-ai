f9a8d5390d17be09af4160058818d74a53a3c5da
Nathan Benzor <n8benzor@gmail.com>
2026-01-28 12:30:28 -0700

    setup basic nav with route to new session using expo router

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    // Stack defines the screen structure for the app
+    <Stack>
+      <Stack.Screen name="index" options={{ title: "Sessions" }} />
+      <Stack.Screen name="new-session" options={{ title: "New Session" }} />
+    </Stack>
+  );
 }
---
+++ app/new-session.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function NewSessionRoute() {
+  return (
+    <View style={{ padding: 16 }}>
+      <Text style={{ fontSize: 20, fontWeight: "600" }}>New Session</Text>
+    </View>
+  );
+}
